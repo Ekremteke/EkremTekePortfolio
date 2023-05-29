510f6c3a2e1f453af6904f02d8e55253640e260b
Ekrem <ekremteke.uk@gmail.com>
2023-05-29 14:45:52 +0100

    cards component was done

--- vertu-motors/src/components/Cards.jsx
+++ vertu-motors/src/components/Cards.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import cardData from '../cardData.json';
+import image1 from '../images/card-m50.png';
+import image2 from '../images/card-m-sport.png';
+import image3 from '../images//card-sport.png';
 
 function Cards() {
+    const images = [image1, image2, image3];
     return (
-        <div className="card-container">
-            {cardData.map((card, index) => (
-                <div className="card" key={index}>
-                    <img className="card-image" src={card.image}
-alt="card-images" />
-                    <div className="card-content">
-                        <h2 className="card-title">{card.name}</h2>
-                        <p className="card-price">{card.price}</p>
-                        <ul className="card-bullets">
-                            {card.bullets.map((bullet, bulletIndex) => (
-                                <li key={bulletIndex}>{bullet}</li>
-                            ))}
-                        </ul>
+        <div className="card-area">
+            <h5>BMW i4 trims</h5>
+            <div className="card-container">
+                {cardData.map((card, index) => (
+                    <div className="card" key={index}>
+                        <img className="card-image" src={images[index]}
+                            alt="card-images" />
+                        <div className="card-content">
+                            <h2 className="card-title">{card.name}</h2>
+                            <p className="card-price">{card.price}</p>
+                            <ul className="card-bullets">
+                                {card.bullets.map((bullet, bulletIndex) => (
+                                    <li key={bulletIndex}>{bullet}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            ))}
+
+                ))}
+            </div>
         </div>
     );
 }
--- vertu-motors/src/components/Cover.jsx
+++ vertu-motors/src/components/Cover.jsx
@@ -17,7 +17,6 @@ function Cover() {
     const handleClose = () => {
         setIsOpen(false);
         setShowConfirmation(false);
-        // Formu sıfırla
         setName('');
         setSurname('');
         setDate('');
@@ -28,80 +27,81 @@ function Cover() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Randevu oluşturma işlemleri burada gerçekleştirilebilir
-
         setShowConfirmation(true);
     };
 
     return (
         <div className="cover-container">
-            <img className="cover-image" src={cover} alt="Logo" />
-            <div className="cover-content">
-                <h1 className="cover-title">Explore the new BMW i4 range</h1>
-                <p className="cover-text">
-                    From the cool Trend and SUV-inspired Active to the sporty ST-Line,
-                    the eighth generation Fiesta impresses across the range.
-                </p>
-                <button className="btn-test-drive" onClick={handleOpen}>
-                    Book a test drive
-                </button>
-                {isOpen && (
-                    <div className="modal">
-                        {!showConfirmation ? (
-                            <div className="modal-content">
-                                <form onSubmit={handleSubmit}>
-                                    <label>
-                                        Name:
-                                        <input
-                                            type="text"
-                                            value={name}
-                                            onChange={(e) => setName(e.target.value)}
-                                        />
-                                    </label>
-                                    <label>
-                                        Surname:
-                                        <input
-                                            type="text"
-                                            value={surname}
-                                            onChange={(e) => setSurname(e.target.value)}
-                                        />
-                                    </label>
-                                    <label>
-                                        Date:
-                                        <input
-                                            type="date"
-                                            value={date}
-                                            onChange={(e) => setDate(e.target.value)}
-                                        />
-                                    </label>
-                                    <label>
-                                        Time:
-                                        <input
-                                            type="time"
-                                            value={time}
-                                            onChange={(e) => setTime(e.target.value)}
-                                        />
-                                    </label>
-                                    <label>
-                                        Email:
-                                        <input
-                                            type="email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                        />
-                                    </label>
-                                    <button type="submit">Submit</button>
-                                </form>
-                                <button onClick={handleClose}>Kapat</button>
-                            </div>
-                        ) : (
-                            <div className="modal-content">
-                                <p>Sayın {name}, {date} {time} saatte test sürüşüne bekliyoruz!</p>
-                                <button onClick={handleClose}>Kapat</button>
-                            </div>
-                        )}
-                    </div>
-                )}
+            <div className="image-container">
+                <img className="cover-image" src={cover} alt="Logo" />
+                <div className="cover-content">
+                    <h3 className="cover-title">Explore the new BMW i4 range</h3>
+                    <p className="cover-text">
+                        From the cool Trend and SUV-inspired Active to the sporty ST-Line,
+                        the eighth generation Fiesta impresses across the range.
+                    </p>
+                    <button className="btn-test-drive" onClick={handleOpen}>
+                        Book a test drive
+                    </button>
+                    {isOpen && (
+                        <div className="modal">
+                            {!showConfirmation ? (
+                                <div className="modal-content">
+                                    <form onSubmit={handleSubmit}>
+                                        <label>
+                                            Name:
+                                            <input
+                                                type="text"
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                            />
+                                        </label>
+                                        <label>
+                                            Surname:
+                                            <input
+                                                type="text"
+                                                value={surname}
+                                                onChange={(e) => setSurname(e.target.value)}
+                                            />
+                                        </label>
+                                        <label>
+                                            Date:
+                                            <input
+                                                type="date"
+                                                value={date}
+                                                onChange={(e) => setDate(e.target.value)}
+                                            />
+                                        </label>
+                                        <label>
+                                            Time:
+                                            <input
+                                                type="time"
+                                                value={time}
+                                                onChange={(e) => setTime(e.target.value)}
+                                            />
+                                        </label>
+                                        <label>
+                                            Email:
+                                            <input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
+                                        </label>
+                                        <button type="submit">Submit</button>
+                                    </form>
+                                    <button onClick={handleClose}>Kapat</button>
+                                </div>
+                            ) : (
+                                <div className="modal-content">
+                                    <p>Sayın {name}, {date} {time} saatte test sürüşüne bekliyoruz!</p>
+                                    <button onClick={handleClose}>Kapat</button>
+                                </div>
+                            )}
+                        </div>
+
+                    )}
+                </div>
             </div>
         </div>
     );
