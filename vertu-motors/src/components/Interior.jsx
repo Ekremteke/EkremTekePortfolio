205ac69ac72e1913379f00e8866b9ec80b681310
Ekrem <ekremteke.uk@gmail.com>
2023-05-29 15:35:47 +0100

    interior comp. was done

--- vertu-motors/src/components/Interior.jsx
+++ vertu-motors/src/components/Interior.jsx
@@ -3,14 +3,17 @@ import image from "../images/interior.png"
 
 function Interior() {
   return (
-      <div className="pink-box">
-          <div className="image-container">
-              <img src={image} alt="Example" />
+      <div className="interior-container">
+          <div className="interior-content-container">
+              <h3 className="interior-title">Explore the new BMW i4 range interior</h3>
+              <p className="interior-text">
+                        From the cool Trend and SUV-inspired Active to the sporty ST-Line,
+                        the eighth generation Fiesta impresses across the range.
+                     </p>
+              <button className="btn-request-video">Request a Personalised Video</button>
           </div>
-          <div className="content-container">
-              <h4>Title</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <button>Button</button>
+          <div className="interior-image-container">
+              <img className="interior-image"src={image} alt="interior-image"/>
           </div>
       </div>
   )
