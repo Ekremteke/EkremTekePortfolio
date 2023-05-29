ded2fa44579a4630a5d0d4c920fd869c9096c553
Ekrem <ekremteke.uk@gmail.com>
2023-05-29 22:15:07 +0100

    interior component was adjusted as mobile

--- src/components/Electrify.jsx
+++ src/components/Electrify.jsx
@@ -31,100 +31,197 @@ function Electrify() {
     };
 
     return (
-        <div className="electrify-container">
-            <div className="electrify-image-container">
-                <img className="electrify-image" src={image} alt="interior-image" />
-            </div>
-            <div className="electrify-content-container">
-                <h3 className="electrify-title">Electrifying performance with the BMW i4</h3>
-                <p className="electrify-text">
-                    From the cool Trend and SUV-inspired Active to the sporty ST-Line,
-                    the eighth generation Fiesta impresses across the range.
-                </p>
-                <div className="time-container">
-                    <div className="electrifying-container">
-                        <h3>02:05 hr</h3>
-                        <p className="text">Charging at home for a Range of 62 miles</p>
-                    </div>
-                    <div className="electrifying-container2">
-                        <h3>365</h3>
-                        <p className="text">Electric range in miles</p>
-                    </div>
+        <div>
+            <div className="electrify-container">
+                <div className="electrify-image-container">
+                    <img className="electrify-image" src={image} alt="interior-image" />
                 </div>
-                <button className="btn-test-drive" onClick={handleOpen}>
-                    Book a test drive
-                </button>
-                {isOpen && (
-                    <div className="modal">
-                        {!showConfirmation ? (
-                            <div className="modal-content">
-                                <form onSubmit={handleSubmit}>
-                                    <label>
-                                        Name:
-                                        <input
-                                            type="text"
-                                            value={name}
-                                            onChange={(e) => setName(e.target.value)}
-                                        />
-                                    </label>
-                                    <br />
-                                    <label>
-                                        Surname:
-                                        <input
-                                            type="text"
-                                            value={surname}
-                                            onChange={(e) => setSurname(e.target.value)}
-                                        />
-                                    </label>
-                                    <br />
-                                    <label>
-                                        Date:
-                                        <input
-                                            type="date"
-                                            value={date}
-                                            onChange={(e) => setDate(e.target.value)}
-                                        />
-                                    </label>
-                                    <br />
-                                    <label>
-                                        Time:
-                                        <input
-                                            type="time"
-                                            value={time}
-                                            onChange={(e) => setTime(e.target.value)}
-                                        />
-                                    </label>
-                                    <br />
-                                    <label>
-                                        Email:
-                                        <input
-                                            type="email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                        />
-                                    </label>
-                                    <br />
-                                    <button className='btn-test-drive' type="submit">Submit</button>
-                                </form>
-                                <br />
-                                <button className='btn-test-drive' onClick={handleClose}>Close</button>
-                            </div>
-                        ) : (
-                            <div className="modal-content">
-                                <p>Dear {name} {surname},
-                                    <br />Your appointment has adjusted on {date} at {time}. We will be happy to see you.
-                                    <br />
-                                    Kind regards,
-                                </p>
-                                <br />
-                                <button className='btn-test-drive' onClick={handleClose}>Close</button>
-                            </div>
-                        )}
+                <div className="electrify-content-container">
+                    <h3 className="electrify-title">Electrifying performance with the BMW i4</h3>
+                    <p className="electrify-text">
+                        From the cool Trend and SUV-inspired Active to the sporty ST-Line,
+                        the eighth generation Fiesta impresses across the range.
+                    </p>
+                    <div className="time-container">
+                        <div className="electrifying-container">
+                            <h3>02:05 hr</h3>
+                            <p className="text">Charging at home for a Range of 62 miles</p>
+                        </div>
+                        <div className="electrifying-container2">
+                            <h3>365</h3>
+                            <p className="text">Electric range in miles</p>
+                        </div>
                     </div>
+                    <button className="btn-test-drive" onClick={handleOpen}>
+                        Book a test drive
+                    </button>
+                    {isOpen && (
+                        <div className="modal">
+                            {!showConfirmation ? (
+                                <div className="modal-content">
+                                    <form onSubmit={handleSubmit}>
+                                        <label>
+                                            Name:
+                                            <input
+                                                type="text"
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Surname:
+                                            <input
+                                                type="text"
+                                                value={surname}
+                                                onChange={(e) => setSurname(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Date:
+                                            <input
+                                                type="date"
+                                                value={date}
+                                                onChange={(e) => setDate(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Time:
+                                            <input
+                                                type="time"
+                                                value={time}
+                                                onChange={(e) => setTime(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Email:
+                                            <input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <button className='btn-test-drive' type="submit">Submit</button>
+                                    </form>
+                                    <br />
+                                    <button className='btn-test-drive' onClick={handleClose}>Close</button>
+                                </div>
+                            ) : (
+                                <div className="modal-content">
+                                    <p>Dear {name} {surname},
+                                        <br />Your appointment has adjusted on {date} at {time}. We will be happy to see you.
+                                        <br />
+                                        Kind regards,
+                                    </p>
+                                    <br />
+                                    <button className='btn-test-drive' onClick={handleClose}>Close</button>
+                                </div>
+                            )}
+                        </div>
 
-                )}
+                    )}
+                </div>
+
             </div>
+            <div className="electrify-container-mobile">
+                <div className="electrify-image-container">
+                    <img className="electrify-image" src={image} alt="interior-image" />
+                </div>
+                <div className="electrify-content-container">
+                    <h3 className="electrify-title">Electrifying performance with the BMW i4</h3>
+                    <p className="electrify-text">
+                        From the cool Trend and SUV-inspired Active to the sporty ST-Line,
+                        the eighth generation Fiesta impresses across the range.
+                    </p>
+                    <div className="time-container">
+                        <div className="electrifying-container">
+                            <h3>02:05 hr</h3>
+                            <p className="text">Charging at home for a Range of 62 miles</p>
+                        </div>
+                        <div className="electrifying-container2">
+                            <h3>365</h3>
+                            <p className="text">Electric range in miles</p>
+                        </div>
+                    </div>
+                    <button className="btn-test-drive" onClick={handleOpen}>
+                        Book a test drive
+                    </button>
+                    {isOpen && (
+                        <div className="modal">
+                            {!showConfirmation ? (
+                                <div className="modal-content">
+                                    <form onSubmit={handleSubmit}>
+                                        <label>
+                                            Name:
+                                            <input
+                                                type="text"
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Surname:
+                                            <input
+                                                type="text"
+                                                value={surname}
+                                                onChange={(e) => setSurname(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Date:
+                                            <input
+                                                type="date"
+                                                value={date}
+                                                onChange={(e) => setDate(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Time:
+                                            <input
+                                                type="time"
+                                                value={time}
+                                                onChange={(e) => setTime(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <label>
+                                            Email:
+                                            <input
+                                                type="email"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
+                                        </label>
+                                        <br />
+                                        <button className='btn-test-drive' type="submit">Submit</button>
+                                    </form>
+                                    <br />
+                                    <button className='btn-test-drive' onClick={handleClose}>Close</button>
+                                </div>
+                            ) : (
+                                <div className="modal-content">
+                                    <p>Dear {name} {surname},
+                                        <br />Your appointment has adjusted on {date} at {time}. We will be happy to see you.
+                                        <br />
+                                        Kind regards,
+                                    </p>
+                                    <br />
+                                    <button className='btn-test-drive' onClick={handleClose}>Close</button>
+                                </div>
+                            )}
+                        </div>
 
+                    )}
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/components/Interior.jsx
+++ src/components/Interior.jsx
@@ -3,6 +3,7 @@ import image from "../images/interior.png"
 
 function Interior() {
   return (
+    <div>
       <div className="interior-container">
           <div className="interior-content-container">
               <h3 className="interior-title">Explore the new BMW i4 range interior</h3>
@@ -15,6 +16,21 @@ function Interior() {
           <div className="interior-image-container">
               <img className="interior-image"src={image} alt="interior-image"/>
           </div>
+      </div>
+          <div className="interior-container-grid">
+              <div className="interior-content-container-grid">
+                  <h1 className="interior-title-grid">Explore the new BMW i4 range interior</h1>
+                  <p className="interior-text-grid">
+                      From the cool Trend and SUV-inspired Active to the sporty ST-Line,
+                      the eighth generation Fiesta impresses across the range.
+                  </p>
+                  <button className="btn-test-drive-mobile2">Request a Personalised Video</button>
+              </div>
+              <div className="interior-image-container-grid">
+                  <img className="interior-image" src={image} alt="interior-image" />
+              </div>
+          </div>
+
       </div>
   )
 }
