e2b74c701079312c7d605d023f5e7240f687e389
Ekrem <ekremteke.uk@gmail.com>
2023-05-29 19:50:08 +0100

    main images and main section were adjusted as responsive

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -34,6 +34,75 @@ function Cover() {
     return (
         <div className="cover-container">
             <div className='cover-mobile'>
+                {isOpen && (
+                    <div className="modal-mobile">
+                        {!showConfirmation ? (
+                            <div className="modal-content">
+                                <form onSubmit={handleSubmit}>
+                                    <label>
+                                        Name:
+                                        <input
+                                            type="text"
+                                            value={name}
+                                            onChange={(e) => setName(e.target.value)}
+                                        />
+                                    </label>
+                                    <br />
+                                    <label>
+                                        Surname:
+                                        <input
+                                            type="text"
+                                            value={surname}
+                                            onChange={(e) => setSurname(e.target.value)}
+                                        />
+                                    </label>
+                                    <br />
+                                    <label>
+                                        Date:
+                                        <input
+                                            type="date"
+                                            value={date}
+                                            onChange={(e) => setDate(e.target.value)}
+                                        />
+                                    </label>
+                                    <br />
+                                    <label>
+                                        Time:
+                                        <input
+                                            type="time"
+                                            value={time}
+                                            onChange={(e) => setTime(e.target.value)}
+                                        />
+                                    </label>
+                                    <br />
+                                    <label>
+                                        Email:
+                                        <input
+                                            type="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                    </label>
+                                    <br />
+                                    <button className='btn-test-drive' type="submit">Submit</button>
+                                </form>
+                                <br />
+                                <button className='btn-test-drive' onClick={handleClose}>Close</button>
+                            </div>
+                        ) : (
+                            <div className="modal-content">
+                                <p>Dear {name} {surname},
+                                    <br />Your appointment has adjusted on {date} at {time}. We will be happy to see you.
+                                    <br />
+                                    Kind regards,
+                                </p>
+                                <br />
+                                <button className='btn-test-drive' onClick={handleClose}>Close</button>
+                            </div>
+                        )}
+                    </div>
+
+                )}
                 <img className="mobile-image" src={mobile} alt="Logo" />
                 <div className="mobile-cover-content">
                     <h1 className="mobile-cover-title">Explore the new BMW i4 range</h1>
@@ -44,75 +113,7 @@ function Cover() {
                     <button className="btn-test-drive-mobile" onClick={handleOpen}>
                         Book a test drive
                     </button>
-                    {isOpen && (
-                        <div className="modal">
-                            {!showConfirmation ? (
-                                <div className="modal-content">
-                                    <form onSubmit={handleSubmit}>
-                                        <label>
-                                            Name:
-                                            <input
-                                                type="text"
-                                                value={name}
-                                                onChange={(e) => setName(e.target.value)}
-                                            />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Surname:
-                                            <input
-                                                type="text"
-                                                value={surname}
-                                                onChange={(e) => setSurname(e.target.value)}
-                                            />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Date:
-                                            <input
-                                                type="date"
-                                                value={date}
-                                                onChange={(e) => setDate(e.target.value)}
-                                            />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Time:
-                                            <input
-                                                type="time"
-                                                value={time}
-                                                onChange={(e) => setTime(e.target.value)}
-                                            />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Email:
-                                            <input
-                                                type="email"
-                                                value={email}
-                                                onChange={(e) => setEmail(e.target.value)}
-                                            />
-                                        </label>
-                                        <br />
-                                        <button className='btn-test-drive' type="submit">Submit</button>
-                                    </form>
-                                    <br />
-                                    <button className='btn-test-drive' onClick={handleClose}>Close</button>
-                                </div>
-                            ) : (
-                                <div className="modal-content">
-                                    <p>Dear {name} {surname},
-                                        <br />Your appointment has adjusted on {date} at {time}. We will be happy to see you.
-                                        <br />
-                                        Kind regards,
-                                    </p>
-                                    <br />
-                                    <button className='btn-test-drive' onClick={handleClose}>Close</button>
-                                </div>
-                            )}
-                        </div>
-
-                    )}
+                   
                 </div>
             </div>
             <div className="image-container">
